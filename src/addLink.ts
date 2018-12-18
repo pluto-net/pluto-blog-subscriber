437e3ca6bfbd646b52a5d92d5ed14beeac2ae43e
Tylor Shin <shincode@icloud.com>
2018-12-18 14:25:40 +0900

    Add blogLinkData put logic in DynamoDB

--- src/addLink.ts
+++ src/addLink.ts
@@ -1,5 +1,8 @@
 "use strict";
 
+import * as uuid from "uuid/v1";
+import DynamoDBManager from "./model";
+
 interface Params {
   link: string;
 }
@@ -29,16 +32,30 @@ export async function addBlogLink(event, _context, _callback) {
     };
   }
 
-  // check it already exists in DB
+  const id = uuid();
 
-  // save it to DB
+  try {
+    await DynamoDBManager.putBlogLink({
+      id: id,
+      link: params.link,
+      active: false
+    });
+  } catch (_err) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Failed to save link item to DynamoDB."
+      })
+    };
+  }
+
+  // check it already exists in DB
   // return the latest list
 
   return {
-    statusCode: 200,
+    statusCode: 400,
     body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
+      message: "You sent malformed request body"
     })
   };
 }
---
+++ src/model/index.ts
@@ -0,0 +1,80 @@
+import * as dynamoose from "dynamoose";
+
+const TABLE_NAME = "blogSubscriberTable";
+
+interface BlogLink {
+  id: string;
+  link: string;
+  active: boolean;
+  startTime?: Date[];
+  endTitme?: Date[];
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const Schema = dynamoose.Schema;
+const blogLinkSchema = new Schema(
+  {
+    id: {
+      type: String,
+      hashKey: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    active: {
+      type: Boolean,
+      required: true
+    },
+    startTime: {
+      type: [Date]
+    },
+    endTime: {
+      type: [Date]
+    }
+  },
+  {
+    timestamps: true,
+    useDocumentTypes: true
+  }
+);
+
+const BlogLinkModel = dynamoose.model<BlogLink, { id: string }>(
+  TABLE_NAME,
+  blogLinkSchema
+);
+
+class DynamoDBManager {
+  async putBlogLink(blogLink: BlogLink) {
+    try {
+      const blogLinkModel = new BlogLinkModel(blogLink);
+
+      const res = await blogLinkModel.save();
+      return res;
+    } catch (err) {
+      console.error("had an error to put link item in DynamoDB.");
+      throw new Error(err);
+    }
+  }
+
+  // async updateDynamoDB(paper: Paper) {
+  //   try {
+  //     await PaperModel.update(
+  //       { paper_id: paper.paperId },
+  //       {
+  //         paper_pdf: paper.paperPdf,
+  //         process_status: paper.processStatus,
+  //         paper_images: paper.paperImages
+  //       }
+  //     );
+  //   } catch (err) {
+  //     console.error("ERROR OCCURRED AT UPDATE DYNAMO_DB ITEM");
+  //     throw new Error(err);
+  //   }
+  // }
+}
+
+const dynamoDBManger = new DynamoDBManager();
+
+export default dynamoDBManger;
