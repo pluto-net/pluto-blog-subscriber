dd40193b3671f2afe72108d2325be6e2f003977b
Tylor Shin <shincode@icloud.com>
2018-12-18 18:54:26 +0900

    Fix body check logic

--- src/getBlogList.ts
+++ src/getBlogList.ts
@@ -3,17 +3,6 @@
 import DynamoDBManager, { BlogLink } from "./model";
 
 export async function getBlogList(event, _context, _callback) {
-  const body = event.body;
-
-  if (!body) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: "No Request Body!"
-      })
-    };
-  }
-
   try {
     const res = await DynamoDBManager.getBlogList();
 
