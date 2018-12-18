5711ac0b8607b0bdc4b788d011d19ca301edc373
Tylor Shin <shincode@icloud.com>
2018-12-18 11:41:30 +0900

    Add no Request Body logic

--- src/addLink.ts
+++ src/addLink.ts
@@ -1,7 +1,24 @@
 "use strict";
 
 export async function addBlogLink(event, _context, _callback) {
-  console.log(event);
+  // getNewLinkFromEventBody();
+  const body = event.body;
+
+  if (!body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "No Request Body!"
+      })
+    };
+  }
+
+  console.log(body);
+
+  // check it already exists in DB
+  // save it to DB
+  // return the latest list
+
   return {
     statusCode: 200,
     body: JSON.stringify({
